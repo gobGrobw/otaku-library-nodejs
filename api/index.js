4cee738a741d4cca5d59fb3954c4ae434dae5218
gobGrobw <albertfoeng12@gmail.com>
2024-08-16 19:35:30 +0700

    removed cors in index

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: '*' }));
 
 app.get('/', async (req, res) => {
 	return res.redirect('/api');
