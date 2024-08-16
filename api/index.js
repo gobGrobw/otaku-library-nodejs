446eb90de88900cd1f73d0a9a5680864270a966e
gobGrobw <albertfoeng12@gmail.com>
2024-08-16 20:18:58 +0700

    allow cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const apiRouter = require('./router');
-const allowCors = require('./allowCors');
 require('./dbConnect');
 
 const app = express();
@@ -8,7 +7,19 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(allowCors);
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method == 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+
+	next();
+});
 
 app.get('/', async (req, res) => {
 	return res.redirect('/api');
