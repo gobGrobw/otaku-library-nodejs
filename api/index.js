e414cf6fdadb187dac66fd00497a036fa12f4774
gobGrobw <albertfoeng12@gmail.com>
2024-08-16 19:30:38 +0700

    updated cors

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.get('/', async (req, res) => {
 	return res.redirect('/api');
