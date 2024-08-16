5007aaea15d80495caf3af0d85123de3be04bb07
gobGrobw <albertfoeng12@gmail.com>
2024-08-16 20:11:11 +0700

    allow cors

--- api/allowCors.js
+++ api/allowCors.js
@@ -1,6 +1,9 @@
 const allowCors = (fn) => async (req, res) => {
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://otaku-library.vercel.app/log-in'
+	);
 	// another common pattern
 	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 	res.setHeader(
