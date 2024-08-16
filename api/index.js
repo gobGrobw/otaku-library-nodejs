8d841cb11433b363eebc3f4942b7448e0e8787d8
gobGrobw <albertfoeng12@gmail.com>
2024-08-16 20:24:46 +0700

    allow cors

--- api/index.js
+++ api/index.js
@@ -7,16 +7,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	);
-	res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-
-	next();
-});
 
 app.get('/', async (req, res) => {
 	return res.redirect('/api');
